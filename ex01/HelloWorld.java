59d52f259a052e7325f43b42ab9edc87c5dcdecc
dbafu <damon.fu@qq.com>
2018-01-05 00:02:46 +0800

    Hello World write in java

--- ex01/HelloWorld.java
+++ ex01/HelloWorld.java
@@ -1,7 +1,13 @@
 public class HelloWorld
+  /* This is a  java helloworld program.
+   * This will print 'Hello World' as a output.
+   * This is an example of multi-line comments.
+   */
 {
   public static void main(String[] args)
   {
+    // This is an example of single line comment
+    /* This is also an example of single line comment. */
     System.out.println("Hello World !");
   }
 }
