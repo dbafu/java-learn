906c248b45bbb2358f8a43e4cbc28df10c89985d
ok2fly <2899109958@qq.com>
2018-01-22 22:07:22 +0800

    variable

---
+++ ex02/Test2.java
@@ -0,0 +1,14 @@
+public class Test2
+{
+    public void pupAge()
+    {
+        int age;
+        age = age + 7;
+        System.out.println("Puppy age is : " + age);
+    }
+
+    public static void main(String[] args) {
+        Test2 test = new Test2();
+        test.pupAge();
+    }
+}
