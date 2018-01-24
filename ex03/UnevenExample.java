1d2393e18ca518047c58b9944638530deb35413d
ok2fly <2899109958@qq.com>
2018-01-24 22:22:44 +0800

    学习进度记录

---
+++ ex03/UnevenExample.java
@@ -0,0 +1,16 @@
+class UnevenExample
+{
+    public static void main(String[] args) {
+        int [][] uneven =
+        {
+            {1, 9, 4},
+            {0, 2},
+            {0, 1, 2, 3, 4}
+        };
+
+        System.out.println("Length of array is : " + uneven.length );
+        System.out.println("Length of array[0] is : " + uneven[0].length );
+        System.out.println("Length of array[1] is : " + uneven[1].length );
+        System.out.println("Length of array[2] is : " + uneven[2].length );
+    }
+}
